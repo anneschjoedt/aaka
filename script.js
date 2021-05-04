2d0af8b93ab7d598d3ea2146b1143941d76b66b9
anneschjoedt <anne.schjoedt@gmail.com>
2021-05-04 10:47:21 +0200

    nye ændringer

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
 function on(id) {
-  document.getElementById(id + "e").style.display = "block";
+  newId = id + "e";
+  document.getElementById(newId).style.display = "block";
 }
 
-function off(id) {
-  document.getElementById(id).style.display = "none";
+function off() {
+  document.getElementById("img-onee").style.display = "none";
+  document.getElementById("img-twoe").style.display = "none";
+  document.getElementById("img-threee").style.display = "none";
+  document.getElementById("img-foure").style.display = "none";
 }
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+
+// Magnifying glass
